e7e97696ba25be709a409894be7769afebcca146
isaac-blasiman <isaac.blasiman@gmail.com>
2018-02-03 15:23:04 -0500

    Unfinished attempt at creating canvas from javascript for better control

--- custom-js-scripts.js
+++ custom-js-scripts.js
@@ -1,4 +1,11 @@
-var myCanvas = document.getElementById("active-signature-box");
+// var myCanvas = document.getElementById("active-signature-box");
+var container = document.getElementById("container");
+var myCanvas = document.createElement("canvas");
+myCanvas.length="200";
+myCanvas.height="400";
+myCanvas.width="400";
+myCanvas.id="active-signature-box";
+container.insertBefore(myCanvas, container.firstChild);
 var canvasContext = myCanvas.getContext("2d");
 
 function ouch(the_element, the_event) {
@@ -9,5 +16,8 @@ function ouch(the_element, the_event) {
     canvasContext.moveTo(0, 0);
     canvasContext.lineTo(mouseX, mouseY)
     canvasContext.stroke();
+    imageData = canvasContext.getImageData(0, 0, 400, 400);
+    console.log(imageData);
+    console.log(typeof imageData);
 }
 // We'll get the mouse position and try to draw where the mouse is going there.

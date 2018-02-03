1c8bae3eaf4c902b81c69d9cc9f656451f9ada8f
isaac-blasiman <isaac.blasiman@gmail.com>
2018-02-02 20:49:48 -0500

    First attempt at changing scripts so that mouse position is used when the canvas is hovered over

--- custom-js-scripts.js
+++ custom-js-scripts.js
@@ -2,8 +2,12 @@ var myCanvas = document.getElementById("active-signature-box");
 var canvasContext = myCanvas.getContext("2d");
 
 function ouch(the_element, the_event) {
+    var mouseX = the_event.clientX;
+    var mouseY = the_event.clientY;
+    alert("mouseX is " + mouseX + ", mouseY is " + mouseY);
     canvasContext.beginPath();
-    canvasContext.rect(10,20,100,50);
+    canvasContext.moveTo(0, 0);
+    canvasContext.lineTo(mouseX, mouseY)
     canvasContext.stroke();
 }
 // We'll get the mouse position and try to draw where the mouse is going there.

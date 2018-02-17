0d839c1a743619bfb0ba712db429d8ca9623f4bb
isaac-blasiman <isaac.blasiman@gmail.com>
2018-02-16 22:50:38 -0500

    non-working code to try to resize the canvas

--- custom-js-scripts.js
+++ custom-js-scripts.js
@@ -2,3 +2,5 @@ var container = document.getElementById("container");
 var myCanvas = document.createElement("canvas");
 container.insertBefore(myCanvas, container.firstChild);
 var signaturePad = new SignaturePad(myCanvas);
+// myCanvas.height = 50vw;
+// myCanvas.width = 50vw;

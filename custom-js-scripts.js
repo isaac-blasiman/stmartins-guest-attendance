9ef816e9320de09bf335dc0b342eabdb956e6887
isaac-blasiman <isaac.blasiman@gmail.com>
2018-02-16 22:09:52 -0500

    Removed my old code and put in a (non-working) call to the signature_pad utility

--- custom-js-scripts.js
+++ custom-js-scripts.js
@@ -1,23 +1,4 @@
-// var myCanvas = document.getElementById("active-signature-box");
 var container = document.getElementById("container");
 var myCanvas = document.createElement("canvas");
-myCanvas.length="200";
-myCanvas.height="400";
-myCanvas.width="400";
-myCanvas.id="active-signature-box";
 container.insertBefore(myCanvas, container.firstChild);
-var canvasContext = myCanvas.getContext("2d");
-
-function ouch(the_element, the_event) {
-    var mouseX = the_event.clientX;
-    var mouseY = the_event.clientY;
-    alert("mouseX is " + mouseX + ", mouseY is " + mouseY);
-    canvasContext.beginPath();
-    canvasContext.moveTo(0, 0);
-    canvasContext.lineTo(mouseX, mouseY)
-    canvasContext.stroke();
-    imageData = canvasContext.getImageData(0, 0, 400, 400);
-    console.log(imageData);
-    console.log(typeof imageData);
-}
-// We'll get the mouse position and try to draw where the mouse is going there.
+var signaturePad = new SignaturePad(myCanvas);

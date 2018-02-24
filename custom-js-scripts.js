9ce89c3c80f5cd4cf4ba500bee7414dade291d83
isaac-blasiman <isaac.blasiman@gmail.com>
2018-02-24 17:22:47 -0500

    Removed javascript code to add canvas since it was interfering with the rest of the page

--- custom-js-scripts.js
+++
@@ -1,6 +0,0 @@
-var container = document.getElementById("container");
-var myCanvas = document.createElement("canvas");
-container.insertBefore(myCanvas, container.firstChild);
-var signaturePad = new SignaturePad(myCanvas);
-// myCanvas.height = 50vw;
-// myCanvas.width = 50vw;

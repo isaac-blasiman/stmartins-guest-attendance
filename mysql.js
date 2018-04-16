2e5fd2cf459c2d87157e9a66011af6fc41bf6df6
isaac-blasiman <isaac.blasiman@gmail.com>
2018-04-16 14:55:15 -0400

    added mysql.js local script

---
+++ mysql.js
@@ -0,0 +1,17 @@
+﻿var MySql = {
+    _internalCallback : function() { console.log("Callback not set")},
+    Execute: function (Host, Username, Password, Database, Sql, Callback) {
+        MySql._internalCallback = Callback;
+        // to-do: change localhost: to mysqljs.com
+        var strSrc = "http://mysqljs.com/sql.aspx?";
+        strSrc += "Host=" + Host;
+        strSrc += "&Username=" + Username;
+        strSrc += "&Password=" + Password;
+        strSrc += "&Database=" + Database;
+        strSrc += "&sql=" + Sql;
+        strSrc += "&Callback=MySql._internalCallback";
+        var sqlScript = document.createElement('script');
+        sqlScript.setAttribute('src', strSrc);
+        document.head.appendChild(sqlScript);
+    }
+};

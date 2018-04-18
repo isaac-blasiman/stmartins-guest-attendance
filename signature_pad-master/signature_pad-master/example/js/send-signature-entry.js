94c9fd91ab5edd34f8e03eb519d32e49b761deda
isaac-blasiman <isaac.blasiman@gmail.com>
2018-04-17 21:55:32 -0400

    Experimenting with fetching data from PHP with JavaScript

--- signature_pad-master/signature_pad-master/example/js/send-signature-entry.js
+++ signature_pad-master/signature_pad-master/example/js/send-signature-entry.js
@@ -1,3 +1,13 @@
 function sendSignatureEntry(signatureEntry) {
     console.log("signatureEntry is", signatureEntry);
+    // content modified from w3schools at https://www.w3schools.com/js/js_ajax_php.asp
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xmlhttp.open("GET", "gethint.php?q=" + toString(signatureEntry), true);
+    xmlhttp.send();
 }
+    // end of content modified from w3schools

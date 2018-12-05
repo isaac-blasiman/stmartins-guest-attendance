e1a8572ef27dc017a6a954e2fc2ffa880170320d
Matt <matt.babb@live.com>
2018-12-04 20:58:33 -0500

    Timezone fix
    Fixes timestamp grabbed by Javascript being in UTC rather than local time.

--- signature_pad-master/signature_pad-master/example/js/create-signature-object.js
+++ signature_pad-master/signature_pad-master/example/js/create-signature-object.js
@@ -1,6 +1,7 @@
 /// create-signature-object.js
 /// Provides functionality to create a signature_entry object with a timestamp, status, and image, given the status and image blob data.
 /// Written by Isaac Blasiman 3/29/18
+/// Edited by Matthew Babb 12/4/18
 
 // Pulls from post by https://stackoverflow.com/users/9951/e-satis on https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file?page=1&tab=votes#tab-top
 
@@ -28,12 +29,31 @@ function getSignatureStatus(siteURL) {
 // RETURNS:
 // Given a blob, creates a custom signature_object data type of the form {timestamp: <timestamp>, status: <string>, image: <blob>};
 function createDataObject(signature_status, Blob) {
-    var current_time = new Date(); // Gets the current time stamp
+    var current_time = convertUTCDateToLocalDate(new Date()); // Gets the current time stamp
     current_time = current_time.toISOString().slice(0, 19).replace('T', ' '); // Converts the timestamp to a SQL-readable format. Modified from Stack Overflow, see Attribution 1 at bottom of file.
     var signature_object = {timestamp: current_time.toString(), status: signature_status, image: Blob};
     return signature_object;
 }
 
+// PARAMETER(S):
+// date: A javascript date object based in UTC time
+// RETURNS:
+// Given a javascript date object, the function will return the location adjusted date object
+// *2 
+function convertUTCDateToLocalDate(date) {
+    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+
+    var offset = date.getTimezoneOffset() / 60;
+    var hours = date.getHours();
+
+    newDate.setHours(hours - offset);
+
+    return newDate;   
+}
+
 // ATTRIBUTIONS
 // 1.  URL: https://stackoverflow.com/questions/5129624/convert-js-date-time-to-mysql-datetime
 //      POSTED BY: https://stackoverflow.com/users/4354249/farside
+// 
+// 2.  URL: https://stackoverflow.com/questions/6525538/convert-utc-date-time-to-local-date-time
+//      POSTED BY: https://stackoverflow.com/users/1022459/adorjan-princz
